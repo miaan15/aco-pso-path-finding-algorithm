b36c286e4a6f0a6aa09192bc1d4bf89a72e40f8b
miaan15 <miaanstarman@gmail.com>
2025-12-11 00:49:25 +0700

    idk

---
+++ src/algorithm/solve/hybrid.rs
@@ -0,0 +1,8 @@
+use crate::algorithm::problem::Problem;
+use bevy::prelude::*;
+
+pub struct HybridStrategy {}
+
+impl HybridStrategy {
+    pub fn path_finding(&self, problem: &Problem) -> Option<Vec<Vec2>> { todo!() }
+}
